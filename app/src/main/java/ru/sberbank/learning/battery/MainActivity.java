5431f90554214c8e5b56de1f1a950ac1100cb450
Eugene K <player766@gmail.com>
2017-04-18 21:01:47 +0200

    temp1

--- app/src/main/java/ru/sberbank/learning/battery/MainActivity.java
+++ app/src/main/java/ru/sberbank/learning/battery/MainActivity.java
@@ -1,13 +1,47 @@
 package ru.sberbank.learning.battery;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private ImageView batteryImageView;
+    private TextView batteryPercentageView;
+
+    private BroadcastReceiver batteryReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        batteryImageView = (ImageView) findViewById(R.id.battery_image);
+        batteryPercentageView = (TextView) findViewById(R.id.battery_percentage);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(batteryReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(batteryReceiver);
     }
 }
